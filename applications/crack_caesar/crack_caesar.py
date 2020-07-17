90ed7942aca7a40cc4fb933da84ebf53eefa9bf1
AnikaZN <anikanacey@gmail.com>
2020-07-16 20:04:10 -0600

    Day 4 work:

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,46 @@
 # decode it.
 
 # Your code here
+import os
+import re
+import string
 
+ordered = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+           'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+with open("ciphertext.txt") as f:
+    text = f.read()
+
+def crack_caesar(text):
+    counts = {}
+
+    words2 = []
+
+    for char in text:
+        if char.isalpha():
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+
+    count_list = list(counts.items())
+    count_list.sort(key=lambda x: -x[1])
+
+    mapping = {}
+
+    for x in range(26):
+        mapping[count_list[x][0]] = ordered[x]
+
+    new_text = []
+
+    for char in text:
+        if char.isalpha():
+            if char in mapping:
+                new_text.append(mapping[char])
+        else:
+            new_text.append(char)
+
+    return "".join(new_text)
+
+
+print(crack_caesar(text))
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,43 @@
 # Your code here
+import os
+import re
+import string
 
+with open("robin.txt") as f:
+    text = f.read()
+
+def hist_words(text_doc):
+    counts = {}
+
+    remove = string.punctuation
+    remove = remove.replace("'", "") # don't remove apostrophes
+    pattern = r"[{}]".format(remove) # create the pattern
+    no_punctuation = re.sub(pattern, " ", text_doc)
+    words = no_punctuation.split()
+
+    words2 = []
+
+    for word in words:
+        word = word.lower()
+        words2.append(word)
+
+    for x in words2:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+
+    count_list = list(counts.items())
+    count_list.sort(key=lambda x: (-x[1], x[0]))
+
+    width = max([len(word[0]) for word in count_list]) + 2
+
+    for tuple in count_list:
+        bar = []
+        bar.append(tuple[0])
+        for x in range(tuple[1]):
+            bar.append('#')
+        print(f'{bar[0].ljust(width)}{"".join(bar[1:])}')
+
+
+print(hist_words(text))
